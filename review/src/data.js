9aa81593e0ec9b47505b8a85efc7c4f79e4db649
DanTling <20010908@st.phenikaa-uni.edu.vn>
2023-06-07 21:48:28 +0700

    Up folder Review

---
+++ review/src/data.js
@@ -0,0 +1,90 @@
+const reviews = [
+    {
+        id: 1,
+        name: 'susan smith',
+        job: 'web developer',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883334/person-1_rfzshl.jpg',
+        text:
+            "I'm baby meggings twee health goth +1. Bicycle rights tumeric chartreuse before they sold out chambray pop-up. Shaman humblebrag pickled coloring book salvia hoodie, cold-pressed four dollar toast everyday carry",
+    },
+    {
+        id: 2,
+        name: 'anna johnson',
+        job: 'web designer',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883409/person-2_np9x5l.jpg',
+        text:
+            'Helvetica artisan kinfolk thundercats lumbersexual blue bottle. Disrupt glossier gastropub deep v vice franzen hell of brooklyn twee enamel pin fashion axe.photo booth jean shorts artisan narwhal.',
+    },
+    {
+        id: 3,
+        name: 'peter jones',
+        job: 'intern',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883417/person-3_ipa0mj.jpg',
+        text:
+            'Sriracha literally flexitarian irony, vape marfa unicorn. Glossier tattooed 8-bit, fixie waistcoat offal activated charcoal slow-carb marfa hell of pabst raclette post-ironic jianbing swag.',
+    },
+    {
+        id: 4,
+        name: 'bill anderson',
+        job: 'the boss',
+        image:
+            'https://res.cloudinary.com/diqqf3eq2/image/upload/v1586883423/person-4_t9nxjt.jpg',
+        text:
+            'Edison bulb put a bird on it humblebrag, marfa pok pok heirloom fashion axe cray stumptown venmo actually seitan. VHS farm-to-table schlitz, edison bulb pop-up 3 wolf moon tote bag street art shabby chic. ',
+    },
+    {
+        id: 5,
+        name: 'David Degea',
+        job: 'Goalkeeper',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p51940.png',
+        text: 'As a goalkeeper, my role is to defend the goal and prevent the opposing team from scoring. I am responsible for controlling the penalty area, communicating with my teammates, and making split-second decisions to stop shots on goal. My job requires quick reflexes, strong communication skills, and the ability to read the game and anticipate the movements of attackers.'
+    },
+    {
+        id: 6,
+        name: 'Tom Heaton',
+        job: 'Goalkeeper',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p21205.png',
+        text: 'As a goalkeeper, my job is to protect my teams goalpost from the opposition teams attacks. It requires quick reflexes, good decision-making skills, and strong communication with my teammates on the field. I need to accurately judge the pace, direction, and trajectory of the incoming ball and make quick decisions to either catch or deflect it to prevent a goal.'
+    }, {
+        id: 7,
+        name: 'Victor Lindelöf',
+        job: 'Defender',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p184667.png',
+        text: 'It also involves staying up-to-date with the latest cyber threats and trends in order to adapt and improve our defense strategies. Ultimately, my job is critical in safeguarding our data, privacy, and financial well-being.'
+    }, {
+        id: 8,
+        name: 'Harry Maguire',
+        job: 'Defender',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p95658.png',
+        text: 'I also respond to incidents and breaches in a timely and effective manner, working closely with other team members to minimize damages and ensure a prompt recovery. Additionally, I stay up-to-date with the latest security trends and technologies, and constantly evaluate and improve our existing security measures to ensure the best possible protection for our organizations assets.'
+    }, {
+        id: 9,
+        name: 'Raphaël Varane',
+        job: 'Defender',
+        image: 'https://th.bing.com/th/id/OIP._5KuWz3ADSail7jCpp4BzwHaFJ?pid=ImgDet&rs=1',
+        text: 'As a Defender in a cybersecurity team, my main role is to protect the organizations resources from cyber attacks and breaches.I work proactively to identify potential vulnerabilities and threats, and develop strategies to mitigate and prevent them.'
+    }, {
+        id: 10,
+        name: 'Lisandro Martínez',
+        job: 'Defender',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p221820.png',
+        text: 'As a goalkeeper, my role is to defend the goal and prevent the opposing team from scoring. I am responsible for controlling the penalty area, communicating with my teammates, and making split-second decisions to stop shots on goal. My job requires quick reflexes, strong communication skills, and the ability to read the game and anticipate the movements of attackers.'
+    }, {
+        id: 11,
+        name: 'Luke Shaw',
+        job: 'Defender',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p106760.png',
+        text: 'I also provide security awareness training to employees to ensure that they understand the importance of cybersecurity and follow best practices. Overall, my job is critical to protecting the organizations digital assets and operations.'
+    }, {
+        id: 12,
+        name: 'Aaron Wan-Bissaka',
+        job: 'Defender',
+        image: 'https://resources.premierleague.com/premierleague/photos/players/250x250/p214590.png',
+        text: 'I work closely with other security professionals to ensure that all vulnerabilities are identified and addressed to prevent unauthorized access, data theft, and other malicious activities. My duties include monitoring security logs, analyzing network traffic, deploying security solutions, conducting vulnerability assessments, and investigating security incidents. '
+    },
+];
+
+export default reviews;
